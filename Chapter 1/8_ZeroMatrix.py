aa1407d9239437197649ed7568d652dbadbc43d4
Yongjip Kim <yongjip@sas.upenn.edu>
2016-08-31 21:37:59 -0400

    solved Q8-9 in Chapter 1

--- Chapter 1/8_ZeroMatrix.py
+++ Chapter 1/8_ZeroMatrix.py
@@ -1,8 +1,21 @@
 # O(MxN)
+'''
+Write an algorithm such that if an element in an MxN matrics is 0, it's entire row and column are set to 0.
+'''
 import unittest
-
+import copy
 
 def zero_matrix(matrix):
+    output = copy.deepcopy(matrix)
+    row_len = len(matrix[0])
+    zero_vector = [0 for _ in range(row_len)]
+    for ii, row_ii in enumerate(matrix):
+        for jj, value_jj in enumerate(row_ii):
+            if value_jj == 0:
+                output[ii] = copy.copy(zero_vector)
+                for row in output:
+                    row[jj] = 0
+    return output
 
 
 class Test(unittest.TestCase):
--- Chapter 1/9_StringRotation.py
+++ Chapter 1/9_StringRotation.py
@@ -1,12 +1,31 @@
 # O(N)
+'''
+Assume you have a method isSubstring which checks if one word is a substring of another. Given two strings, s1 and s2, write code to check if s2 is a rotation of s1 using only one call to isSubstring("waterbottle" if a rotation of "erbottlewat")
+'''
+
 import unittest
 
 def string_rotation(s1, s2):
+    if len(s1) != len(s2):
+        return False
+    for i in range(len(s1)):
+        start_part = s1[:i]
+        end_part = s1[i:]
+        if isSubstring(s2, start_part) and isSubstring(s2, end_part):
+            return True
+    return False
+
+def isSubstring(string, sub):
+    if string.find(sub) == -1:
+        return False
+    return True
+
 
 class Test(unittest.TestCase):
     '''Test Cases'''
     data = [
         ('waterbottle', 'erbottlewat', True),
+        ('water','', False)
         ('foo', 'bar', False),
         ('foo', 'foofoo', False)
     ]
